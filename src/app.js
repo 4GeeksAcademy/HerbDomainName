06773f6e9463e080418f2e3c7594a70c60515b92
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-04-24 22:50:45 +0000

    Finished successfully!

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let domainNames = [];
+
+let pronouns = ["the", "our", "my", "your"];
+let adjectives = ["great", "big", "small", "tiny"];
+let nouns = ["jogger", "racoon", "dog", "cat"];
+
+for (let i = 0; i < pronouns.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < nouns.length; k++) {
+      let domainName = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+      domainNames.push(domainName);
+      console.log(domainName);
+    }
+  }
+}
